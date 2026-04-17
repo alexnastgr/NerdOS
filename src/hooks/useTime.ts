336bdac4331ee04a425716f0d78bfa36b272381b
alexnastgr <alexanstgr@gmail.com>
2026-04-18 00:48:26 +0300

    feature: display 12 or 24 hour formated time

--- src/hooks/useTime.ts
+++ src/hooks/useTime.ts
@@ -1,8 +1,22 @@
 import { useEffect, useState } from "react";
-import { formatedTime } from "@/helpers/formator";
+import { useSettings } from "./useSettings";
 
 const useTime = () => {
+  const { timeFormat } = useSettings(); // 12 ή 24
   const [time, setTime] = useState<Date>(new Date());
+
+  const formatedTime = (date: Date, format: number): string => {
+    let hours = date.getHours();
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+
+    if (format === 12) {
+      const ampm = hours >= 12 ? "PM" : "AM";
+      hours = hours % 12 || 12;
+      return `${String(hours).padStart(2, "0")}:${minutes} ${ampm}`;
+    }
+
+    return `${String(hours).padStart(2, "0")}:${minutes}`;
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -12,9 +26,9 @@ const useTime = () => {
     return () => clearInterval(interval);
   }, []);
 
-  const localTime = formatedTime(time);
+  const localTime = formatedTime(time, timeFormat);
 
-  return { localTime };
+  return { time, localTime, timeFormat };
 };
 
 export default useTime;
